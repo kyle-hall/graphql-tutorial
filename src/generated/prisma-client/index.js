1da343d3c59e2ab6e5606c71ac4f0d5272344b8f
Kyle Hall <khall@pillartechnology.com>
2019-06-19 13:44:26 -0500

    adding the generated stuff

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/kyle-hall-76ed92/hackernews-node-tut/dev`
+});
+exports.prisma = new exports.Prisma();
